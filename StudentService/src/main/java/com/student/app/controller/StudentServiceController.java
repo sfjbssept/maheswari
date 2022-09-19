323715cf0c5b2ac4e7a8d8e2e9c1a6ea351db60f
sfjbssept <sfjbssept@gmail.com>
2022-09-19 03:53:59 +0000

    School MS service commit

---
+++ StudentService/src/main/java/com/student/app/controller/StudentServiceController.java
@@ -0,0 +1,52 @@
+package com.student.app.controller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.student.app.entity.Student;
+
+@RestController
+public class StudentServiceController {
+
+	private static Map<String, List<Student>> schoolDB = new HashMap<String, List<Student>>();
+	
+	static {
+		schoolDB = new HashMap<String, List<Student>>();
+		
+		List<Student> lst = new ArrayList<>();
+		Student std =new Student("Nishant", "Class12");
+		lst.add(std);
+		std=new Student("vinay", "Class11");
+		lst.add(std);
+		schoolDB.put("school1", lst);
+		
+		List<Student> lst1 = new ArrayList<>();
+		Student std1 =new Student("Mahes", "Class10");
+		lst1.add(std1);
+		std1=new Student("Mugil", "Class10");
+		lst1.add(std1);
+		schoolDB.put("school2", lst1);
+		
+	}
+	
+	@RequestMapping(value = "/getStudentDetailsForSchool/{schoolname}", method=RequestMethod.GET)
+	public List <Student> getStudents(@PathVariable String schoolname){
+		System.out.println("Getting student details for : "+schoolname);
+		List<Student> studentList = schoolDB.get(schoolname);
+		if (studentList == null) {
+			studentList =new ArrayList<Student>();
+			Student std = new Student("Not found 404","N/A");
+			studentList.add(std);
+		}
+		
+		return studentList;
+		
+	}
+}
